336eece0fec5ef940178364fbfecc06b20db6c69
waldinho <james.waller1984@gmail.com>
2017-03-29 00:17:48 +0100

    files added

---
+++ charts.js
@@ -0,0 +1,195 @@
+document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.html'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.css'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.frameworks'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.pre'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.node'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.react'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.mongo'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.git'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.npm'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.grunt'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.cms'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.api'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+        var chart = window.chart = new EasyPieChart(document.querySelector('.adobe'), {
+            easing: 'easeOutElastic',
+            delay: 3000,
+            barColor: '#FF7575',
+            trackColor: '#e8e0ff',
+            scaleColor: false,
+            lineWidth: 20,
+            trackWidth: 16,
+            lineCap: 'butt',
+            onStep: function(from, to, percent) {
+                this.el.children[0].innerHTML = Math.round(percent);
+            }
+        });
+    });
